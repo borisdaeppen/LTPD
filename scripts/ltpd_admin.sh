eb6545c188686e218290efe8dae0802cdc367fdd
Boris Däppen <bo@1204u.(none)>
2014-06-02 16:16:50 +0200

    added small system for storing table passwords (to detect pw-typos)

---
+++ scripts/ltpd_admin.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CONFIG_PATH="./config"
+
+ARG=$1
+
+if [ "$ARG" = "inittable" ]
+then
+    echo -n "password table: "
+    read TABLE_NAME
+    echo -n "password: "
+    read TABLE_PW
+    PW_HASH=$(mysql -uroot -p -s -r -N -e "SELECT SHA2('$TABLE_PW', 512)")
+    echo $PW_HASH > "$CONFIG_PATH/$TABLE_NAME.sha2"
+else
+   echo "Use 'help' for instructions on this command"
+fi
